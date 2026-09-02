b0db342c5a12a729235ac3d51a0b7655b1bdba35
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-02 23:06:31 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,16 @@
+/**
+ * Program menyapa pengguna.
+ */
+public class day 1 {
+
+    // Komentar satu baris
+
+    public static void main(String[] args) {
+
+        /*
+         * Komentar beberapa baris
+         */
+
+        System.out.println("Halo, Dunia!");
+    }
+}
