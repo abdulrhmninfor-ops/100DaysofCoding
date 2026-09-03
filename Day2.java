23ea3c0778df47c705ad98e563413f8293fe3f7a
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-03 11:02:03 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,41 @@
+/**
+ * App
+ */
+public class Day2 {
+    public static void main(String[] args) {
+        // Data diri saya
+        String nama = "Abdul rahman";
+        String alamat = "Polewali";
+        String umur = "19 tahun";
+        String jurusan = "Informatika";
+        String nim = "D0226035";
+
+        //Menggunakan println
+        System.out.println("======BIODATA DIRI=====");
+        System.out.println("nama \t\t:" + nama);
+        System.out.println("alamt \t\t:" + alamat);
+        System.out.println("umur \t\t:" + umur);
+        System.out.println("jurusan \t:" + jurusan);
+        System.out.println("nim \t\t:" + nim);
+
+        System.out.println("\n");
+
+        //Menggunakan print
+        System.out.print("nama \t\t:" + nama + "\n");
+        System.out.print("alamat \t\t:" + alamat + "\n");
+        System.out.print("umur \t\t:"+ umur + "\n");
+        System.out.print("jurusan \t:" + jurusan + "\n");
+        System.out.println("nim \t\t:" + nim + "\n");
+
+        System.out.println("\n");
+
+        //Menggunakan printf
+        System.out.printf("nama \t\t: %s\n", nama);
+        System.out.printf("alamt \t\t: %s\n", alamat);
+        System.out.printf("umur \t\t: %s\n", umur);
+        System.out.printf("jurusan \t: %s\n", jurusan);
+        System.out.printf(" nim \t\t: $s\n", nim);
+    }
+
+    
+}
