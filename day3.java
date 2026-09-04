1ca3f2079de0a8090a1fab07cf16109fa4c33e12
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-04 16:23:18 +0800

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,29 @@
+public class day3 {
+    
+    public static void main(String[] args) {
+        // \n new line, untuk membuat baris baru 
+        System.out.println("====Biodata Diri====");
+        // \t tap, digunakan untuk memberi jarak seperti tombol tap
+        System.out.println("nama \t : Abdul rahman");
+        System.out.println("Umur \t : 18 tahun");
+        System.out.println("Alamat \t : Polewali");
+        System.out.println("======================");
+        // \b Backspace, Digunakan untuk mundur satu posisi karakter, seperti tombol Backspace.
+        System.out.println("Hala\bo!");
+        // \" petik dua, untuk menampilkan kutip dua 
+        System.out.println("\"saya lagi tes ngoding selama 100 hari\"");
+        // \' Petik satu, untuk menampilkan kutip satu 
+        System.out.println("saya sangat suka belajar \'ngoding\'");
+        // \\ Backlash, Digunakan untuk menampilkan karakter backslash 
+        System.out.println("Nama File: C:\\Data\\Biodata.txt");
+        // \f Form Feed perpindahan halaman pada perangkat/lingkungan yang mendukungnya.
+        System.out.println("Halaman 1\fHalaman 2");
+        // \r Carriage Return, Digunakan untuk kembali ke awal baris.
+        System.out.print("Status: Belum");
+        System.out.print("\rStatus: Selesai");
+        
+        
+        
+    }
+    
+}
