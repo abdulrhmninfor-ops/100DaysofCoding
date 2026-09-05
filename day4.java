391276b8ae3244947ecffca6cf813f36c0e200d4
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-05 15:04:42 +0800

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,43 @@
+public class day4 {
+    
+    public static void main(String[] args) {
+        
+        // byte berfungsi menyimpan bilangan bulat dengan ukuran atau nilai kecil, misalnya jumlah barang yang sedikit.
+        byte jumlahPensil = 20;
+        
+        /*
+         * short berfungsi menyimpan bilangan bulat yang lebih besar
+         * dibandingkan byte.
+         * misalnya jumlah kursi dalam satu sekolah.
+         */
+        short jumlahKursi = 550;
+        
+        /* 
+         * int berfungsi menyimpan bilangan bulat yang umum di gunakan.
+         * misalnya nilai uang dalam Rupiah
+         */
+        int uang = 120000;
+        
+        /* 
+         * long berfungsi menyimpan bilangan bulat
+         * dengan nilai yang sangat besar 
+         * misalnya jumlah penduduk dalam suatu negara
+         */
+         long jumlahPenduduk = 1460000000;
+        
+        // Menampilkan nilai dari setiap tipe data ke layar.
+        System.out.println("jumlahPensil \t:" + jumlahPensil);
+        System.out.println("jumlahKursi \t:" + jumlahKursi);
+        System.out.println("uang \t\t:Rp" + uang);
+        System.out.println("jumlahPendudk \t:" + jumlahPenduduk);
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
