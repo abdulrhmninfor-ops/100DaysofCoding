437190def16aa78f0b595414598e6dbd0a6ae8d0
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-07 20:40:36 +0800

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,60 @@
+public class day6 {
+    
+    public static void main(String[] args) {
+        /**
+         * Tipe data karakter adalah jenis data yang digunakan untuk menyimpan
+         * satu karakter tunggal, seperti huruf, angka, atau simbol.
+         * Jadi, karakter hanya mewakili satu tanda atau satu tulisan, bukan kumpulan kata.
+         * tipe data karakter yaitu char dan boolean
+         */
+        
+        /** char adalah tipe data untuk menyimpan satu karakter
+         *  seperti huruf, angka, atau simbol. 
+         *  Karakter ditulis menggunakan tanda petik satu (' ').
+         */
+        
+        char huruf = 'R';
+        char angka = '7';
+        char simbol = '?';
+        
+        // contoh lain
+        char jenisKelamin = 'L';
+        char golonganDara = 'o';
+        
+        
+        /** 
+         * boolean adalah tipe data yang digunakan untuk menyimpan dua kondisi
+         * yaitu benar (true) atau salah (false).
+         */
+         
+        boolean akunAktif = true;
+        boolean hadir = false;
+        // Menentukan apakah akun masih aktif 
+        
+        // tipe data char
+        System.out.println("===========data char============");
+        System.out.println("huruf \t\t\t:" + huruf);
+        System.out.println("angka \t\t\t:" + angka);
+        System.out.println("simbol \t\t\t:" + simbol);
+        
+        System.out.println("jenis kelamin \t\t:" + jenisKelamin);
+        System.out.println("golongan dara \t\t:" + golonganDara +"+");
+        System.out.println("================================" +"\n");
+        
+        // tipe data boolean
+        System.out.println("==========data boolean==========");
+        System.out.println("akun aktif \t\t:" + akunAktif);
+        System.out.println("hsdir  \t\t\t:" + hadir);
+        System.out.println("================================");
+        System.out.println("==========Day6 Selesai==========");
+        
+
+        
+        
+        
+        
+        
+        
+    }
+    
+}
