e4caf0330948a71fd55bfdbd7ed3ec28c023e639
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-08 06:52:49 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,42 @@
+public class day7 {
+    
+    public static void main(String[] args) {
+        
+        // string di gunakan untuk menyimpan teks 
+        // dan juga bisa untuk huruf, kata, simbol, atau kalimat
+        // bisa berisi angka, tapi jika angkanya perlu di hitung sebaiknya pakai tipe data numerik
+        
+        String nama = "Abdul Rahman";
+        String umur = "18 tahun";
+        String alamat = "Jl. Poros mamasa. Kel. Darma";
+        String hobi = "Renang";
+        String nomor = "085342819881";
+        
+        // menampilkan isi String 
+        System.out.println("===Data Diri===");
+        System.out.println("Nama \t:" + nama);
+        System.out.println("Umur \t:" + umur);
+        System.out.println("Alamat\t:" + alamat);
+        System.out.println("Hobi \t:" + hobi);
+        System.out.println("Nomor \t:" + nomor);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
