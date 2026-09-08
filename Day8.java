31489353d876a858a29c36ea9da199a636ca383a
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-09 07:33:22 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,44 @@
+public class Day8 {
+    
+    public static void main(String[] args) {
+        
+        // "Deklarasi" adalah proses membuat sebuah variabel
+        // dengan menentukan tipe data dan nama variabelnya.
+        // Tipe data "int" digunakan karena saldo pada contoh ini
+        // disimpan dalam bentuk bilangan bulat.
+        // tetapi nilai belum di tuliskan
+        
+        int saldo;
+        
+        // inisialisasi memberikan nilai awal kepada variabel.
+        
+        saldo = 200000;
+        
+        // Menampilkan nilai saldo awal ke layar.
+        System.out.println("saldo awal \t: Rp" + saldo);
+        
+        // Update adalah proses mengubah nilai variabel
+        // yang sebelumnya sudah memiliki nilai.
+        // mengubah nilai saldo yang sebelumnya Rp200.000 
+        // menjadi Rp250.000
+        // saldo tidak perlu di deklarasikan, karna sudah di buat sebelumnya 
+        
+        saldo = 250000;
+        
+        // menampilkan nilai saldo setelah mengalami perubahan atau setelh di ubdate.
+        System.out.println("saldo setelah ubdate : Rp" + saldo);
+        
+        
+        /* 
+         * Note 
+         *  Deklarasi = membuat variabel dan menentukan tipe data
+         *  Inisialisasi = memberi nilai awal
+         *  Update = mengubah nilai yang sudah ada 
+         */
+        
+        
+        
+        
+    }
+    
+}
