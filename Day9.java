2e25d42889653ccbfa27f74ea4d6501d3ccb82f3
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-10 11:52:14 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,29 @@
+    
+    public static void main(String[] args) {
+        
+        /*
+         * final/konstanta  digunakan untuk membuat variabel 
+         * yang nilainya tidak bisa diubah lagi setelah diberi nilai
+         */
+        
+        final String NAMA_SEKOLAH = "UPTD SMAN 3 Polewali";
+        final int JUMLAH_SISWA = 900;
+        
+        // menampilkan hasil 
+        System.out.println("Nama Sekolah \t:" + NAMA_SEKOLAH);
+        System.out.println("Jumlah Siswa \t:" + JUMLAH_SISWA);
+        
+        /*
+         * Note
+         * Nama variabel tidak wajib huruf kapital
+         * tetapi untuk membedan bahwa nilai itu bersifat final tidak bisah di ubah
+         * juga agar konstanta mudah di kenali
+         */
+        
+        // Final String, untuk teks tetap
+        // Final int, untuk bilangan bulat tetap
+        
+        
+    }
+    
+}
