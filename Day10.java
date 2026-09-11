8293dd687546a43cd5a6f333392e50db6db799e6
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-11 18:21:22 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,94 @@
+public class Day10 {
+    
+    public static void main(String[] args) {
+        
+        // BYTE
+        // byte digunakan untuk menyimpan bilangan bulat yang kecil.
+        // Nilai paling kecil byte adalah -128.
+        // Nilai paling besar byte adalah 127.
+        
+        System.out.println("BYTE");
+        System.out.println("Minimal \t:" + Byte.MIN_VALUE);
+        System.out.println("Maksimal \t:" + Byte.MAX_VALUE +"\n");
+        
+        // SHORT
+        // short digunakan untuk menyimpan bilangan bulat
+        // yang lebih besar daripada byte.
+        // Nilai paling kecil adalah -32.768.
+        // Nilai paling besar adalah 32.767.
+        
+        System.out.println("SHORT");
+        System.out.println("Minimal \t:"+ Short.MIN_VALUE); 
+        System.out.println("Maksimal \t:"+ Short.MAX_VALUE +"\n");
+        
+        // INT
+        // int digunakan untuk menyimpan bilangan bulat.
+        // Tipe data ini sering digunakan untuk umur, jumlah barang,
+        // tahun, dan berbagai angka bulat lainnya.
+        // Nilai paling kecil adalah -2.147.483.648.
+        // Nilai paling besar adalah 2.147.483.647.
+        
+        System.out.println("INT");
+        System.out.println("Minimal \t: " + Integer.MIN_VALUE);
+        System.out.println("Maksimal \t: " + Integer.MAX_VALUE +"\n");
+        
+        // LONG
+        // long digunakan untuk menyimpan bilangan bulat
+        // yang nilainya sangat besar.
+        // Batasnya lebih besar daripada int.
+        System.out.println("LONG");
+        System.out.println("Minimal \t: " + Long.MIN_VALUE);
+        System.out.println("Maksimal \t: " + Long.MAX_VALUE +"\n");
+        
+        // FLOAT
+        // float digunakan untuk menyimpan angka pecahan/desimal.
+        // Contohnya: 3.14, 10.5, dan 75.5.
+        System.out.println("FLOAT");
+        System.out.println("Nilai positif terkecil : " + Float.MIN_VALUE);
+        System.out.println("Nilai maksimal        \t: " + Float.MAX_VALUE +"\n");
+        
+        // DOUBLE
+        // double digunakan untuk menyimpan bilangan pecahan/desimal.
+        // double memiliki kapasitas lebih besar dan lebih teliti
+        // dibandingkan float.
+        // Nilai maksimal sekitar 1,7 × 10^308.
+        System.out.println("DOUBLE");
+        System.out.println("Nilai positif terkecil \t: " + Double.MIN_VALUE);
+        System.out.println("Nilai maksimal        \t: " + Double.MAX_VALUE +"\n");
+
+        // CHAR
+        // char digunakan untuk menyimpan satu karakter.
+        // Nilainya berupa bilangan Unicode dari 0 sampai 65.535.
+        System.out.println("CHAR");
+        System.out.println("Minimal : " + (int) Character.MIN_VALUE);
+        System.out.println("Maksimal: " + (int) Character.MAX_VALUE +"\n");
+        \\ (int) digunakan untuk mengubah sementara nilai char menjadi angka int, 
+        \\ supaya yang ditampilkan adalah kode angkanya.
+        
+        
+        
+        // BOOLEAN
+        // boolean tidak memiliki batas angka seperti tipe data lainnya.
+        // boolean hanya memiliki dua nilai:
+        // true  = benar
+        // false = salah
+        System.out.println("BOOLEAN");
+        System.out.println("Nilai: true atau false");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
