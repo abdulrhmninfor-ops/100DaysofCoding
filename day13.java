bb30592499302fbb1bb051e7700950099e0bcd48
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-14 16:51:33 +0800

    Create day13.java

---
+++ day13.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class latihan5 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukan nama: ");
+        String nama = input.nextLine();
+        
+        
+        System.out.print("Masukan Nama: ");
+        String namaPanggilan = input.nextLine();
+        
+        
+        System.out.print("Masukan Umur: ");
+        int umur = input.nextInt();
+        
+        System.out.print("Masukan Tingi Badan: ");
+        double tinggiBadan = input.nextDouble();
+        
+        System.out.println("Nama saya :" + nama);
+        System.out.println("Nama  Bisa di panggil :" + nama);
+        System.out.println("Umur saya :" + umur);
+        System.out.println("Tinggi badan saya :" + tinggiBadan);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+          }
