670e16a0613e47fcbf8e86b8a2c20badcec379c6
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-15 19:21:12 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,18 @@
+public class day14 {
+    
+    public static void main(String[] args) {
+        
+        int angka1 = 40;
+        int angka2 = 10;
+        int angka3 = 5;
+        
+        
+        System.out.println("========== Day 14 ===========");
+        System.out.println("Hasil Penjumlahan \t:" + (angka1+angka2+angka3));
+        System.out.println("Hasil Pengurangan \t:" + (angka1-angka2-angka3));
+        
+        
+        
+    }
+    
+}
