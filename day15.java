5f4c43a885071b8cb8668b03a3ef8c656c7f2248
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-16 15:33:27 +0800

    Create day15.java

---
+++ day15.java
@@ -0,0 +1,26 @@
+// Operator aritmatika perkalian (*) dan pembagian (/)
+public class Day15 {
+    
+    public static void main(String[] args) {
+        
+        int a = 50;
+        int b = 5;
+        
+        // menghitung perkalian
+        int hasilPerkalian = a*b;
+        // menghitung pembagian
+        int hasilPembagian = a/b;
+        
+        
+        // menampilkan hasil
+        System.out.println("========== Day 15 ===========");
+        System.out.println("Perkalian \t: " + hasilPerkalian);
+        System.out.println("Pembagian \t: " + hasilPembagian);
+        System.out.println("=============================");
+        
+        
+        
+        
+    }
+    
+}
