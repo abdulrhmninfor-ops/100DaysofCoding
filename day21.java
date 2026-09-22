38d98ee901a636378ec94954ab5eadc026a55827
abdulrhmninfor-ops <abdulrhmninfor@gmail.com>
2026-09-22 23:39:55 +0800

    Create day21.java

---
+++ day21.java
@@ -0,0 +1,17 @@
+public class day21 {
+    
+    public static void main(String[] args) {
+        
+        String a = "25"; 
+
+        // String diubah menjadi int
+        int b = Integer.parseInt(teks); 
+
+        System.out.println("nilai String = " + a);
+        System.out.println("nilai Int = " + b);
+        
+        
+        
+    }
+    
+}
